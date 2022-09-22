b99082f307c496abd2c7c5a6c66da0026ec6df92
Pathfinder_FUFU <49439351+server00987@users.noreply.github.com>
2022-09-22 10:57:08 +0800

    Add files via upload

---
+++ Type191LanguageLibrary.lua
@@ -0,0 +1,343 @@
+--[[
+Valid Languages: bg cs da de el en en-PT es-ES et fi fr ga-IE he hr hu it ja ko lt nl no pl pt-BR pt-PT ru sk sv-SE th tr uk vi zh-CN zh-TW
+You must use one of the above.
+
+Translation Guideline
+1. The language that be translated is your native language.
+2. Only translate formally. Do not translate with slang, improper grammar, spelling, etc.
+3. Please use English instead if untranslated or untranslatable.
+--]]
+
+if (SERVER) then return end
+
+local T191LangLib = {
+	["en"] = { -- English
+		-- Skins
+		["T191_LangLib_Skin.Category"] = "Skins",
+		["T191_LangLib_Skin.Name_1"] = "Asiimov",
+		["T191_LangLib_Skin.Name_2"] = "Unicorn",
+		["T191_LangLib_Skin.Name_3"] = "Carbon Fiber Decals",
+		-- Barrels
+		["T191_LangLib_Barrel.Category"] = "Barrels",
+		["T191_LangLib_Barrel.Nmae_1"] = "QBU-191 Barrel",
+		["T191_LangLib_Barrel.Nmae_2"] = "Short QBU-191 Barrel",
+		["T191_LangLib_Barrel.Nmae_3"] = "QBZ-192 Barrel",
+		-- Handguards
+		["T191_LangLib_Handguard.Category"] = "Handguards",
+		["T191_LangLib_Handguard.Name_1"] = "QBU-191 Handguard",
+		["T191_LangLib_Handguard.Name_2"] = "Tactical Handguard",
+		["T191_LangLib_Handguard.Name_3"] = "Short Tactical Handguard",
+		-- Pistol Grip
+		["T191_LangLib_Pistol_Grip.Category"] = "Pistol Grip",
+		["T191_LangLib_Pistol_Grip.Name"] = "Tactical Pistol Grip",
+		-- Stocks
+		["T191_LangLib_Stock.Category"] = "Stocks",
+		["T191_LangLib_Stock.Name_1"] = "Stock Close",
+		["T191_LangLib_Stock.Name_2"] = "Tactical Stock",
+		["T191_LangLib_Stock.Name_3"] = "Tactical Stock Close",
+		-- Magazines
+		["T191_LangLib_Magazines.Category"] = "Magazines",
+		["T191_LangLib_Magazines.Name_1"] = "45 Round Mags",
+		["T191_LangLib_Magazines.Name_2"] = "QBZ-95 Magazine",
+		["T191_LangLib_Magazines.Name_3"] = "QBZ-95 40 Round Mags",
+		-- Muzzle Devices
+		["T191_LangLib_Muzzle_Devices.Category"] = "Muzzle Devices",
+		["T191_LangLib_Muzzle_Devices.Name"] = "Big Bore Suppressor",
+		-- Lasers
+		["T191_LangLib_Laser.Category"] = "Lasers",
+		["T191_LangLib_Laser.Name_1"] = "LS-2L1 - Red",
+		["T191_LangLib_Laser.Name_2"] = "LS-2L1 - Green",
+		["T191_LangLib_Laser.Name_3"] = "LS-2L1 v2 - Red",
+		["T191_LangLib_Laser.Name_4"] = "LS-2L1 v2 - Green",
+		["T191_LangLib_Laser.Name_5"] = "MINI SCOUT LIGHT® PRO",
+	    -- Charms
+		["T191_LangLib_Charm.Category"] = "Charms",
+		["T191_LangLib_Charm.Terraria"] = "Terraria Charms",
+		["T191_LangLib_Charm.Terraria_Tip_1"] = "Music Has Stopped.",
+		["T191_LangLib_Charm.Terraria_Tip_2"] = "You're now listening the music from Terraria.",
+		["T191_LangLib_Charm.Terraria_Tip_3"] = "You can type !stcst in your chat to stop the music.",
+		["T191_LangLib_Charm.Terraria_Plantera_Name_1"] = "Plantera - First Form",
+		["T191_LangLib_Charm.Terraria_Plantera_Name_2"] = "Plantera - Second Form",
+		["T191_LangLib_Charm.Terraria_Skeletron_Prime_Name_1"] = "Skeletron Prime",
+		["T191_LangLib_Charm.Terraria_Skeletron_Prime_Name_2"] = "Skeletron Prime - Broken",
+		-- Gadgets
+		["T191_LangLib_Gadgets.Category"] = "Gadgets",
+		["T191_LangLib_Gadgets.Name"] = "Heartbeat Sensor",
+		-- Sights
+		["T191_LangLib_Sight.Category"] = "Sights",
+		-- Setting Menu
+		["T191_LangLib_SettingMenu.Title_1"] = "Notification",
+		["T191_LangLib_SettingMenu.ControlHelp_1"] = "Enable the text notification of the chatbox.",
+		["T191_LangLib_SettingMenu.Title_2"] = "Charm Music",
+		["T191_LangLib_SettingMenu.ControlHelp_2"] = "Enable the music of the charm.",
+		["T191_LangLib_SettingMenu.Title_3"] = "Random Skin",
+		["T191_LangLib_SettingMenu.ControlHelp_3"] = "Enable the random skin of the weapon.",
+		-- Fire Mode
+		["T191_LangLib_FireMode.Full_Auto"] = "Full Auto",
+		["T191_LangLib_FireMode.Semi_Auto"] = "Semi Auto",
+	},
+	["zh-CN"] = { -- Simplified Chinese
+		-- Skins
+		["T191_LangLib_Skin.Category"] = "皮肤",
+		["T191_LangLib_Skin.Name_1"] = "二西莫夫",
+		["T191_LangLib_Skin.Name_2"] = "独角兽",
+		["T191_LangLib_Skin.Name_3"] = "碳纤维贴纸",
+		-- Barrels
+		["T191_LangLib_Barrel.Category"] = "枪管",
+		["T191_LangLib_Barrel.Nmae_1"] = "QBU-191枪管",
+		["T191_LangLib_Barrel.Nmae_2"] = "截短QBU-191枪管",
+		["T191_LangLib_Barrel.Nmae_3"] = "QBZ-192枪管",
+		-- Handguards
+		["T191_LangLib_Handguard.Category"] = "护木",
+		["T191_LangLib_Handguard.Name_1"] = "QBU-191护木",
+		["T191_LangLib_Handguard.Name_2"] = "战术护木",
+		["T191_LangLib_Handguard.Name_3"] = "截短战术护木",
+		-- Pistol Grip
+		["T191_LangLib_Pistol_Grip.Category"] = "手枪式握把",
+		["T191_LangLib_Pistol_Grip.Name"] = "战术手枪式握把",
+		-- Stocks
+		["T191_LangLib_Stock.Category"] = "枪托",
+		["T191_LangLib_Stock.Name_1"] = "闭合枪托",
+		["T191_LangLib_Stock.Name_2"] = "战术枪托",
+		["T191_LangLib_Stock.Name_3"] = "闭合战术枪托",
+		-- Magazines
+		["T191_LangLib_Magazines.Category"] = "弹匣",
+		["T191_LangLib_Magazines.Name_1"] = "扩容弹匣(45发)",
+		["T191_LangLib_Magazines.Name_2"] = "QBZ-95弹匣",
+		["T191_LangLib_Magazines.Name_3"] = "QBZ-95扩容弹匣(40发)",
+		-- Muzzle Devices
+		["T191_LangLib_Muzzle_Devices.Category"] = "枪口",
+		["T191_LangLib_Muzzle_Devices.Name"] = "大口径消音器",
+		-- Lasers
+		["T191_LangLib_Laser.Category"] = "镭射器",
+		["T191_LangLib_Laser.Name_1"] = "LS-2L1 - 红",
+		["T191_LangLib_Laser.Name_2"] = "LS-2L1 - 绿",
+		["T191_LangLib_Laser.Name_3"] = "LS-2L1 v2 - 红",
+		["T191_LangLib_Laser.Name_4"] = "LS-2L1 v2 - 绿",
+		["T191_LangLib_Laser.Name_5"] = "MINI SCOUT LIGHT® PRO",
+	    -- Charms
+	    ["T191_LangLib_Charm.Category"] = "挂饰",
+		["T191_LangLib_Charm.Terraria"] = "泰拉瑞亚挂饰",
+		["T191_LangLib_Charm.Terraria_Tip_1"] = "音乐已停止播放。",
+		["T191_LangLib_Charm.Terraria_Tip_2"] = "您正在收听到的是来自泰拉瑞亚的音乐。",
+		["T191_LangLib_Charm.Terraria_Tip_3"] = "您可以在聊天框中输入!stcst让音乐停止播放。",
+		["T191_LangLib_Charm.Terraria_Plantera_Name_1"] = "世纪之花 - 第一形态",
+		["T191_LangLib_Charm.Terraria_Plantera_Name_2"] = "世纪之花 - 第二形态",
+		["T191_LangLib_Charm.Terraria_Skeletron_Prime_Name_1"] = "机械骷髅王",
+		["T191_LangLib_Charm.Terraria_Skeletron_Prime_Name_2"] = "机械骷髅王 - 破损",
+		-- Gadgets
+		["T191_LangLib_Gadgets.Category"] = "特殊附件",
+		["T191_LangLib_Gadgets.Name"] = "心跳传感器",
+		-- Sights
+		["T191_LangLib_Sight.Category"] = "瞄准镜",
+		-- Setting Menu
+		["T191_LangLib_SettingMenu.Title_1"] = "提示",
+		["T191_LangLib_SettingMenu.ControlHelp_1"] = "启用聊天框文字提示。",
+		["T191_LangLib_SettingMenu.Title_2"] = "挂饰音乐",
+		["T191_LangLib_SettingMenu.ControlHelp_2"] = "启用挂饰音乐。",
+		["T191_LangLib_SettingMenu.Title_3"] = "随机皮肤",
+		["T191_LangLib_SettingMenu.ControlHelp_3"] = "启用武器随机皮肤。",
+		-- Fire Mode
+		["T191_LangLib_FireMode.Full_Auto"] = "全自动",
+		["T191_LangLib_FireMode.Semi_Auto"] = "半自动",
+	},
+	["zh-TW"] = { -- Traditional Chinese
+		-- Skins
+		["T191_LangLib_Skin.Category"] = "造型",
+		["T191_LangLib_Skin.Name_1"] = "二西莫夫",
+		["T191_LangLib_Skin.Name_2"] = "獨角獸",
+		["T191_LangLib_Skin.Name_3"] = "碳纖維貼膜",
+		-- Barrels
+		["T191_LangLib_Barrel.Category"] = "槍管",
+		["T191_LangLib_Barrel.Nmae_1"] = "QBU-191槍管",
+		["T191_LangLib_Barrel.Nmae_2"] = "短QBU-191槍管",
+		["T191_LangLib_Barrel.Nmae_3"] = "QBZ-192槍管",
+		-- Handguards
+		["T191_LangLib_Handguard.Category"] = "護木",
+		["T191_LangLib_Handguard.Name_1"] = "QBU-191護木",
+		["T191_LangLib_Handguard.Name_2"] = "戰術護木",
+		["T191_LangLib_Handguard.Name_3"] = "短戰術護木",
+		-- Pistol Grip
+		["T191_LangLib_Pistol_Grip.Category"] = "手槍式握把",
+		["T191_LangLib_Pistol_Grip.Name"] = "戰術手槍式握把",
+		-- Stocks
+		["T191_LangLib_Stock.Category"] = "槍托",
+		["T191_LangLib_Stock.Name_1"] = "閉合槍托",
+		["T191_LangLib_Stock.Name_2"] = "戰術槍托",
+		["T191_LangLib_Stock.Name_3"] = "閉合式戰術托",
+		-- Magazines
+		["T191_LangLib_Magazines.Category"] = "彈匣",
+		["T191_LangLib_Magazines.Name_1"] = "擴充弹匣(45發)",
+		["T191_LangLib_Magazines.Name_2"] = "QBZ-95弹匣",
+		["T191_LangLib_Magazines.Name_3"] = "QBZ-95擴充弹匣(40發)",
+		-- Muzzle Devices
+		["T191_LangLib_Muzzle_Devices.Category"] = "槍口",
+		["T191_LangLib_Muzzle_Devices.Name"] = "大口徑抑制器",
+		-- Lasers
+		["T191_LangLib_Laser.Category"] = "雷射指示器",
+		["T191_LangLib_Laser.Name_1"] = "LS-2L1 - 紅",
+		["T191_LangLib_Laser.Name_2"] = "LS-2L1 - 綠",
+		["T191_LangLib_Laser.Name_3"] = "LS-2L1 v2 - 紅",
+		["T191_LangLib_Laser.Name_4"] = "LS-2L1 v2 - 綠",
+		["T191_LangLib_Laser.Name_5"] = "MINI SCOUT LIGHT® PRO",
+	    -- Charms
+	    ["T191_LangLib_Charm.Category"] = "吊飾",
+		["T191_LangLib_Charm.Terraria"] = "泰拉瑞亞吊飾",
+		["T191_LangLib_Charm.Terraria_Tip_1"] = "音樂已停止播放。",
+		["T191_LangLib_Charm.Terraria_Tip_2"] = "您正在收聽到的是來自泰拉瑞亞的音樂。",
+		["T191_LangLib_Charm.Terraria_Tip_3"] = "您可以在聊天框中输入!stcst讓音樂停止播放。",
+		["T191_LangLib_Charm.Terraria_Plantera_Name_1"] = "世紀之花 - 第一形態",
+		["T191_LangLib_Charm.Terraria_Plantera_Name_2"] = "世紀之花 - 第二形態",
+		["T191_LangLib_Charm.Terraria_Skeletron_Prime_Name_1"] = "機械骷髏王",
+		["T191_LangLib_Charm.Terraria_Skeletron_Prime_Name_2"] = "機械骷髏王 - 破損",
+		-- Gadgets
+		["T191_LangLib_Gadgets.Category"] = "特殊配件",
+		["T191_LangLib_Gadgets.Name"] = "心跳感應器",
+		-- Sights
+		["T191_LangLib_Sight.Category"] = "瞄準鏡",
+		-- Setting Menu
+		["T191_LangLib_SettingMenu.Title_1"] = "提示",
+		["T191_LangLib_SettingMenu.ControlHelp_1"] = "開啟聊天欄文字提示。",
+		["T191_LangLib_SettingMenu.Title_2"] = "吊飾音樂",
+		["T191_LangLib_SettingMenu.ControlHelp_2"] = "開啟吊飾音樂。",
+		["T191_LangLib_SettingMenu.Title_3"] = "隨機造型",
+		["T191_LangLib_SettingMenu.ControlHelp_3"] = "啟用武器隨機造型。",
+		-- Fire Mode
+		["T191_LangLib_FireMode.Full_Auto"] = "全自動",
+		["T191_LangLib_FireMode.Semi_Auto"] = "半自動",
+	},
+	["ru"] = { -- Russian
+		-- Skins
+		["T191_LangLib_Skin.Category"] = "Стиль",
+		["T191_LangLib_Skin.Name_1"] = "Азимов",
+		["T191_LangLib_Skin.Name_2"] = "Единорог",
+		["T191_LangLib_Skin.Name_3"] = "Наклейки из углеволокна",
+		-- Barrels
+		["T191_LangLib_Barrel.Category"] = "Ствол",
+		["T191_LangLib_Barrel.Nmae_1"] = "Ствол QBU-191",
+		["T191_LangLib_Barrel.Nmae_2"] = "Короткий ствол QBU-191",
+		["T191_LangLib_Barrel.Nmae_3"] = "Ствол QBZ-192",
+		-- Handguards
+		["T191_LangLib_Handguard.Category"] = "Рукоять",
+		["T191_LangLib_Handguard.Name_1"] = "Рукоять QBU-191",
+		["T191_LangLib_Handguard.Name_2"] = "Тактическая рукоять",
+		["T191_LangLib_Handguard.Name_3"] = "Короткая тактическая рукоять",
+		-- Pistol Grip
+		["T191_LangLib_Pistol_Grip.Category"] = "Пистолетная рукоять",
+		["T191_LangLib_Pistol_Grip.Name"] = "Тактическая пистолетная рукоять",
+		-- Stocks
+		["T191_LangLib_Stock.Category"] = "Приклад",
+		["T191_LangLib_Stock.Name_1"] = "Закрытие приклад",
+		["T191_LangLib_Stock.Name_2"] = "Тактический приклад",
+		["T191_LangLib_Stock.Name_3"] = "Тактический закрытый приклад",
+		-- Magazines
+		["T191_LangLib_Magazines.Category"] = "Магазин",
+		["T191_LangLib_Magazines.Name_1"] = "45 патронный магазин",
+		["T191_LangLib_Magazines.Name_2"] = "Магазин QBZ-95",
+		["T191_LangLib_Magazines.Name_3"] = "45 патронный магазин QBZ-95",
+		-- Muzzle Devices
+		["T191_LangLib_Muzzle_Devices.Category"] = "Дуло",
+		["T191_LangLib_Muzzle_Devices.Name"] = "Глушитель для большого ствола",
+		-- Lasers
+		["T191_LangLib_Laser.Category"] = "Лазер",
+		["T191_LangLib_Laser.Name_1"] = "LS-2L1 - красный",
+		["T191_LangLib_Laser.Name_2"] = "LS-2L1 - зелёный",
+		["T191_LangLib_Laser.Name_3"] = "LS-2L1 v2 - красный",
+		["T191_LangLib_Laser.Name_4"] = "LS-2L1 v2 - зелёный",
+		["T191_LangLib_Laser.Name_5"] = "MINI SCOUT LIGHT® PRO",
+	    -- Charms
+		["T191_LangLib_Charm.Category"] = "Брелок",
+		["T191_LangLib_Charm.Terraria"] = "Брелок из Terraria",
+		["T191_LangLib_Charm.Terraria_Tip_1"] = "Музыка остановилась.",
+		["T191_LangLib_Charm.Terraria_Tip_2"] = "Теперь вы слушаете музыку из Terraria.",
+		["T191_LangLib_Charm.Terraria_Tip_3"] = "Вы можете написать !stcst в чате, чтобы остановить музыку.",
+		["T191_LangLib_Charm.Terraria_Plantera_Name_1"] = "Плантера - первая форма",
+		["T191_LangLib_Charm.Terraria_Plantera_Name_2"] = "Плантера - вторая форма",
+		["T191_LangLib_Charm.Terraria_Skeletron_Prime_Name_1"] = "Скелетрон Прайм",
+		["T191_LangLib_Charm.Terraria_Skeletron_Prime_Name_2"] = "Скелетрон Прайм - сломанный",
+		-- Gadgets
+		["T191_LangLib_Gadgets.Category"] = "Приспособление",
+		["T191_LangLib_Gadgets.Name"] = "Датчик сердцебиения",
+		-- Sights
+		["T191_LangLib_Sight.Category"] = "Прицел",
+		-- Setting Menu
+		["T191_LangLib_SettingMenu.Title_1"] = "Уведомление",
+		["T191_LangLib_SettingMenu.ControlHelp_1"] = "Включение текстового уведомления в окне чата.",
+		["T191_LangLib_SettingMenu.Title_2"] = "Музыка для брелка",
+		["T191_LangLib_SettingMenu.ControlHelp_2"] = "Включение музыки для брелка.",
+		["T191_LangLib_SettingMenu.Title_3"] = "Случайный стиль",
+		["T191_LangLib_SettingMenu.ControlHelp_3"] = "Включение случайного стиля оружия.",
+		-- Fire Mode
+		["T191_LangLib_FireMode.Full_Auto"] = "Автоматич.",
+		["T191_LangLib_FireMode.Semi_Auto"] = "Полуавтомат.",
+	},
+	["sv-SE"] = { -- Swedish
+		-- Skins
+		["T191_LangLib_Skin.Category"] = "Lackeringar",
+		["T191_LangLib_Skin.Name_1"] = "Asiimov",
+		["T191_LangLib_Skin.Name_2"] = "Enhörning",
+		["T191_LangLib_Skin.Name_3"] = "Kolfiber Dekaler",
+		-- Barrels
+		["T191_LangLib_Barrel.Category"] = "Pipor",
+		["T191_LangLib_Barrel.Nmae_1"] = "QBU-191 Pipa",
+		["T191_LangLib_Barrel.Nmae_2"] = "Kort QBU-191 Pipa",
+		["T191_LangLib_Barrel.Nmae_3"] = "QBZ-192 Pipa",
+		-- Handguards
+		["T191_LangLib_Handguard.Category"] = "Handskydd",
+		["T191_LangLib_Handguard.Name_1"] = "QBU-191 Handskydd",
+		["T191_LangLib_Handguard.Name_2"] = "Taktiskt Handskydd",
+		["T191_LangLib_Handguard.Name_3"] = "Kort Taktiskt Handskydd",
+		-- Pistol Grip
+		["T191_LangLib_Pistol_Grip.Category"] = "Pistolgrepp",
+		["T191_LangLib_Pistol_Grip.Name"] = "Taktiskt Pistolgrepp",
+		-- Stocks
+		["T191_LangLib_Stock.Category"] = "Kolv",
+		["T191_LangLib_Stock.Name_1"] = "Stängd Kolv",
+		["T191_LangLib_Stock.Name_2"] = "Taktisk Kolv",
+		["T191_LangLib_Stock.Name_3"] = "Stängd Taktisk Kolv",
+		-- Magazines
+		["T191_LangLib_Magazines.Category"] = "Magasin",
+		["T191_LangLib_Magazines.Name_1"] = "45-Skott Magasin",
+		["T191_LangLib_Magazines.Name_2"] = "QBZ-95 Magasin",
+		["T191_LangLib_Magazines.Name_3"] = "40-Skott QBZ-95 Mag.",
+		-- Muzzle Devices
+		["T191_LangLib_Muzzle_Devices.Category"] = "Mynningsenheter",
+		["T191_LangLib_Muzzle_Devices.Name"] = "Big Bore-Ljuddämpare",
+		-- Lasers
+		["T191_LangLib_Laser.Category"] = "Laser",
+		["T191_LangLib_Laser.Name_1"] = "LS-2L1 - Röd",
+		["T191_LangLib_Laser.Name_2"] = "LS-2L1 - Grön",
+		["T191_LangLib_Laser.Name_3"] = "LS-2L1 v2 - Röd",
+		["T191_LangLib_Laser.Name_4"] = "LS-2L1 v2 - Grön",
+		["T191_LangLib_Laser.Name_5"] = "MINI SCOUT LIGHT® PRO",
+	    -- Charms
+		["T191_LangLib_Charm.Category"] = "Smycken",
+		["T191_LangLib_Charm.Terraria"] = "Terraria Smycken",
+		["T191_LangLib_Charm.Terraria_Tip_1"] = "Musik har stoppats.",
+		["T191_LangLib_Charm.Terraria_Tip_2"] = "Du lyssnar nu på musik från Terraria.",
+		["T191_LangLib_Charm.Terraria_Tip_3"] = "Skriv in !stcst i chatten för att stoppa musiken.",
+		["T191_LangLib_Charm.Terraria_Plantera_Name_1"] = "Plantera - Första Formen",
+		["T191_LangLib_Charm.Terraria_Plantera_Name_2"] = "Plantera - Andra Formen",
+		["T191_LangLib_Charm.Terraria_Skeletron_Prime_Name_1"] = "Skeletron Prime",
+		["T191_LangLib_Charm.Terraria_Skeletron_Prime_Name_2"] = "Skeletron Prime - Sönder",
+		-- Gadgets
+		["T191_LangLib_Gadgets.Category"] = "Tillbehör",
+		["T191_LangLib_Gadgets.Name"] = "Hjärtslagssensor",
+		-- Sights
+		["T191_LangLib_Sight.Category"] = "Sikten",
+		-- Setting Menu
+		["T191_LangLib_SettingMenu.Title_1"] = "Notifikation",
+		["T191_LangLib_SettingMenu.ControlHelp_1"] = "Aktivera text notifikationer i chatten.",
+		["T191_LangLib_SettingMenu.Title_2"] = "Smycke-Musik",
+		["T191_LangLib_SettingMenu.ControlHelp_2"] = "Aktivera smyckens musik.",
+		["T191_LangLib_SettingMenu.Title_3"] = "Slumpmässig Lackering",
+		["T191_LangLib_SettingMenu.ControlHelp_3"] = "Aktivera att slumpmässiga lackeringar utrustas.",
+		-- Fire Mode
+		["T191_LangLib_FireMode.Full_Auto"] = "Helautomat",
+		["T191_LangLib_FireMode.Semi_Auto"] = "Halvautomat",
+	},
+}
+
+local userlang = GetConVar("gmod_language"):GetString() local lang = "en"
+if userlang != nil and istable(T191LangLib[userlang]) then lang = GetConVar("gmod_language"):GetString() end
+for holder, text in pairs(T191LangLib[lang]) do language.Add(holder, text) end
